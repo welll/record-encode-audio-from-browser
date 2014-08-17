06a9a3194530a52f7d67eabc2c599ecb9c7b707d
Wellington Soares <wellington.soares@pixeon.com>
2014-08-17 13:41:06 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,45 @@
+
+# Record/Encode Audio from Browser
+
+
+## A sample demonstrating how to encode the output of Web Audio API nodes.
+
+
+# Compatibility - Browser
+* Chrome 32+
+* Others?
+
+# Codecs
+* MP3: 22.050Hz && 128Kbps using the ported liblame
+* Ogg-Vorbis: @TODO
+* Spx: @TODO
+* Flac: @TODO
+
+#Author
+
+Wellington Soares well.cco@gmail.com
+
+
+#Thanks
+The code is based on the following implementations: 
+
+https://github.com/nusofthq/Recordmp3js 
+https://github.com/akrennmair/speech-to-server
+https://github.com/remusnegrota/Recorderjs
+
+#Contribution
+
+Any contribution will be welcome!
+
+#License
+
+The MIT License (MIT)
+
+Copyright (c) 2014 welll
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ js/enc/mp3/mp3Worker.js
@@ -0,0 +1,124 @@
+importScripts('libmp3lame.js');
+
+var mp3codec;
+
+var recLength = 0,
+	recBuffers = [];
+
+//https://github.com/gypified/libmp3lame/blob/master/API
+
+self.onmessage = function(e) {
+
+	switch (e.data.command) {
+
+		case 'init':
+			init(e.data.config);
+			break;
+		case 'encode':
+			encode(e);
+			break;
+		case 'finish':
+			finish();
+			break;
+	}
+};
+
+function init(config) {
+
+	if (!config) {
+		config = {};
+	}
+
+	mp3codec = Lame.init();
+
+	Lame.set_mode(mp3codec, config.mode || Lame.JOINT_STEREO);
+	Lame.set_num_channels(mp3codec, config.channels || 2);
+	Lame.set_out_samplerate(mp3codec, config.samplerate || 44100);
+	Lame.set_in_samplerate(mp3codec, config.in_samplerate || 44100);
+	/*The default is a  J-Stereo, 44.1khz, 128kbps CBR mp3 file at quality 5.*/
+	Lame.set_bitrate(mp3codec, config.bitrate || 128);
+
+	//Lame.set_mode(mp3codec, 1);
+	//lame.set_quality(mp3codec,2);   /* 2=high  5 = medium  7=low */ 
+	//Lame.set_num_samples(mp3codec, e.data.config.samples || -1);
+
+	Lame.init_params(mp3codec);
+
+	// console.log('Version :', Lame.get_version() + ' / ',
+	// 	'Mode: ' + Lame.get_mode(mp3codec) + ' / ',
+	// 	'Samples: ' + Lame.get_num_samples(mp3codec) + ' / ',
+	// 	'Channels: ' + Lame.get_num_channels(mp3codec) + ' / ',
+	// 	'Input Samplate: ' + Lame.get_in_samplerate(mp3codec) + ' / ',
+	// 	'Output Samplate: ' + Lame.get_in_samplerate(mp3codec) + ' / ',
+	// 	'Bitlate :' + Lame.get_bitrate(mp3codec) + ' / ',
+	// 	'VBR :' + Lame.get_VBR(mp3codec));
+
+}
+
+function encode(e) {
+
+	var mp3data = Lame.encode_buffer_ieee_float(mp3codec, e.data.buf, e.data.buf);
+
+	self.postMessage({
+		command: 'data',
+		buf: mp3data.data
+	});
+
+	console.log(mp3data.data.toString('hex'));
+
+	recBuffers.push(mp3data.data);
+	recLength += mp3data.data.length;
+
+}
+
+function finish() {
+
+	console.log('recBuffers.length:' + recBuffers.length);
+	console.log('recLength:' + recLength);
+
+	var mp3data = Lame.encode_flush(mp3codec);
+
+	recBuffers.push(mp3data.data);
+	recLength += mp3data.data.length;
+
+	self.postMessage({
+		command: 'data',
+		buf: mp3data.data
+	});
+
+	var buffer = mergeBuffers(recBuffers, recLength);
+	//buffer = new ArrayBuffer(buffer.length);
+	var view = new Uint8Array(buffer);
+
+	var audioBlob = new Blob([view], {
+		type: "audio/mp3"
+	});
+
+	self.postMessage({
+		command: 'mp3',
+		buf: audioBlob
+	});
+
+	Lame.close(mp3codec);
+	mp3codec = null;
+
+	recBuffers = [];
+	recLength = {};
+}
+
+function mergeBuffers(recBuffers, recLength) {
+	//Float32Array
+	var result = new Float32Array(recLength);
+
+	var offset = 0;
+
+	for (var i = 0; i < recBuffers.length; i++) {
+
+		result.set(recBuffers[i], offset);
+		offset += recBuffers[i].length;
+
+	}
+
+	return result;
+
+}
