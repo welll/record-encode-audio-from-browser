06dee6d4f22e9c0e5be8ee8c1795e19f3cf1b402
Wellington Soares <wellington.soares@pixeon.com>
2014-08-18 18:40:45 -0300

    Add Gh-Page

---
+++ js/recorder.js
@@ -0,0 +1,159 @@
+(function(window) {
+
+  var recordWavWorker = new Worker('js/enc/wav/recorderWorker.js');
+  var encoderMp3Worker = new Worker('js/enc/mp3/mp3Worker.js');
+
+  var Recorder = function(source) {
+
+    var bufferLen = 4096;
+    var recording = false;
+
+    this.context = source.context;
+
+    /*
+      ScriptProcessorNode createScriptProcessor (optional unsigned long bufferSize = 0,
+       optional unsigned long numberOfInputChannels = 2, optional unsigned long numberOfOutputChannels = 2 );
+    */
+
+    this.node = (this.context.createScriptProcessor || this.context.createJavaScriptNode).call(this.context, bufferLen, 1, 1);
+    this.node.connect(this.context.destination); //this should not be necessary
+
+    this.node.onaudioprocess = function(e) {
+
+      if (!recording)
+        return;
+
+      var channelLeft = e.inputBuffer.getChannelData(0);
+
+      console.log('onAudioProcess' + channelLeft.length);
+
+      encoderMp3Worker.postMessage({
+        command: 'encode',
+        buf: channelLeft
+      });
+
+      recordWavWorker.postMessage({
+        command: 'record',
+        buffer: channelLeft
+      });
+
+    }
+
+    source.connect(this.node);
+
+    this.record = function() {
+
+      if (recording)
+        return false;
+
+      recording = true;
+
+      var sampleRate = this.context.sampleRate;
+
+      console.log("Initializing WAV");
+      log.innerHTML += "\n" + "Creating Empty WAV";
+
+      recordWavWorker.postMessage({
+        command: 'init',
+        config: {
+          sampleRate: sampleRate
+        }
+      });
+
+      console.log("Initializing to Mp3");
+      log.innerHTML += "\n" + "Creating Empty Mp3:" + sampleRate;
+
+      encoderMp3Worker.postMessage({
+        command: 'init',
+        config: {
+          channels: 1,
+          mode: 3 /* means MONO*/ ,
+          samplerate: 22050,
+          bitrate: 64,
+          insamplerate: sampleRate
+        }
+      });
+
+    }
+
+    this.stop = function() {
+
+      if (!recording)
+        return;
+
+      recordWavWorker.postMessage({
+        command: 'finish'
+      });
+
+      encoderMp3Worker.postMessage({
+        command: 'finish'
+      });
+
+      recording = false;
+
+    }
+
+    encoderMp3Worker.onmessage = function(e) {
+
+      var command = e.data.command;
+
+      console.log('encoderMp3Worker - onmessage: ' + command);
+
+      switch (command) {
+        case 'data':
+          var buf = e.data.buf;
+          console.log('Receiving data from mp3-Encoder');
+
+          //maybe you want to send to websocket channel, as:
+          //https://github.com/akrennmair/speech-to-server
+
+          break;
+        case 'mp3':
+          var buf = e.data.buf;
+          endFile(buf, 'mp3');
+          encoderMp3Worker.terminate();
+          encoderMp3Worker = null;
+          break;
+      }
+
+    };
+
+    recordWavWorker.onmessage = function(e) {
+
+      var command = e.data.command;
+
+      console.log('recordWavWorker - onmessage: ' + command);
+
+      switch (command) {
+        case 'wav':
+          endFile(e.data.buf, 'wav');
+          break;
+      }
+
+    };
+
+    function endFile(blob, extension) {
+
+      console.log("Done converting to " + extension);
+      log.innerHTML += "\n" + "Done converting to " + extension;
+
+      console.log("the blob " + blob + " " + blob.size + " " + blob.type);
+
+      var url = URL.createObjectURL(blob);
+      var li = document.createElement('li');
+      var hf = document.createElement('a');
+      hf.href = url;
+      hf.download = new Date().toISOString() + '.' + extension;
+      hf.innerHTML = hf.download;
+      li.appendChild(hf);
+      recordingslist.appendChild(li);
+
+
+    }
+
+
+  };
+
+  window.Recorder = Recorder;
+
+})(window);
